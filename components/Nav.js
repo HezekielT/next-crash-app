9ce09b40d3fa4a2fa9376fe126ac767ba50853a0
hizkielT <hezekiel.feyissa@gmail.com>
2022-10-21 23:48:05 +0300

    navbar component

---
+++ components/Nav.js
@@ -0,0 +1,15 @@
+import navStyles from '../styles/Nav.modulel.css'
+
+function Nav(props) {
+  return (
+    <nav className={navStyles.nav}>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
